81ace0f161c80e06a7782371a96dc0c649875982
Nevanoman <nevanoman@yandex.ru>
2023-08-01 20:52:20 +0300

    fix js

--- js/main.js
+++ js/main.js
@@ -1,64 +1,65 @@
-const swiper = new Swiper('.swiper', {
+const options = {
+	speed: 1000,
+	loop: true,
+	updateOnWindowResize: true,
+
 	breakpoints: {
 		0: {
 			slidesPerView: 1,
 			enabled: true,
 		},
-		768: {
-			enabled: false,
-		}
-	},
-	breakpoints: {
-		0: {
-				slidesPerView: 1,
-				enabled: true,
-		},
 		320: {
-				slidesPerView: 1.3,
-				enabled: true,
+			slidesPerView: 1.3,
+			enabled: true,
 		},
 		360: {
-				slidesPerView: 1.47,
-				enabled: true,
+			slidesPerView: 1.47,
+			enabled: true,
 		},
 		390: {
-				slidesPerView: 1.6,
-				enabled: true,
+			slidesPerView: 1.6,
+			enabled: true,
 		},
 		420: {
-				slidesPerView: 2,
-				enabled: true,
+			slidesPerView: 2,
+			enabled: true,
 		},
 		768: {
-				enabled: false,
-		}
-},
+			enabled: false,
+		},
+	},
 
 	pagination: {
-		el: '.swiper-pagination',
+		el: ".swiper-pagination",
 	},
-});
+};
 
-let showMore = document.querySelector('.show-more');
+let swiper = new Swiper(".swiper", options);
 
-showMore.addEventListener('click', () => {
+let showMore = document.querySelector(".show-more");
+
+showMore.addEventListener("click", () => {
 	let hiddenButton = document.querySelectorAll('[data-hidden="true"]');
 	let hiddenPcButton = document.querySelectorAll('[data-hidden-pc="true"]');
 
-	hiddenButton.forEach(element => {
-		element.classList.toggle('hidden');
+	hiddenButton.forEach((element) => {
+		element.classList.toggle("hidden");
 	});
-	hiddenPcButton.forEach(element => {
-		element.classList.toggle('hidden-pc');
+
+	hiddenPcButton.forEach((element) => {
+		element.classList.toggle("hidden-pc");
 	});
 
 	if (showMore.classList.contains("show-more--decoration-rotate")) {
 		showMore.textContent = "Показать все";
 		showMore.classList.remove("show-more--decoration-rotate");
-
 	} else {
 		showMore.textContent = "Скрыть";
 		showMore.classList.add("show-more--decoration-rotate");
 	}
+});
 
-})
+window.addEventListener("orientationchange", () => {
+	swiper.destroy();
+	swiper = new Swiper(".swiper", options);
+});
